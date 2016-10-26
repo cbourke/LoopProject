c224bc32c3f17e4e285eb7cea841d428ae2a4637
Christopher Bourke <cbourke@cse.unl.edu>
2016-10-25 21:07:31 -0500

    added hint/outline for guessing game

--- exercises/guess.js
+++ exercises/guess.js
@@ -2,12 +2,19 @@
 
 var min = 1;
 var max = 100;
-var x = Math.floor(Math.random() * (max + 1 - min)) + min;
+//create a random number between 1 and 100
+var mysteryNumber = Math.floor(Math.random() * (max + 1 - min)) + min;
 
+//prompt the user for input
 var stringGuess = prompt("enter a value");
-//values from a prompt are strings that need to be
-//parsed to integers:
-var g = parseInt(stringGuess);
+//parse the user's guess as an integer
+var guess = parseInt(stringGuess);
 var numGuesses = 1;
 
 //TODO: finish the program...
+//Outline/hint:
+//while the user's guess is not correct...
+//  inform them if they need to guess higher or lower
+//  prompt them for another input and process it
+//when the user has guessed correctly, give them a
+//congratulations and tell them how many guesses it took.
